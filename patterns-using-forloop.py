886fae46e02585b82527ba1eeb86a5a285a1962a
bassslapper504 <85190497+bassslapper504@users.noreply.github.com>
2021-08-23 17:15:35 +0530

    Create patterns-using-forloop.py

---
+++ patterns-using-forloop.py
@@ -0,0 +1,123 @@
+# sol1
+for r in range (1,4):
+    for c in range (1,4):
+        print("* ",end='')
+    print()
+
+# sol2
+for r in range(1,4):
+    for c in range(1,4):
+        print(r, end='')
+    print()
+
+# sol3
+for r in range(1,4):
+    for c in range(1,4):
+        print(c, end='')
+    print()
+
+# sol4
+for r in range(1,5):
+    for c in range(1,r+1):
+        print("*", end='')
+    print()
+# sol5
+for r in range(1,5):
+    for c in range(1,r+1):
+        print(c, end='')
+    print()
+# sol6
+# sol1
+for r in range (1,4):
+    for c in range (1,4):
+        print("* ",end='')
+    print()
+
+print()
+# sol2
+for r in range(1,4):
+    for c in range(1,4):
+        print(r, end='')
+    print()
+print()
+# sol3
+for r in range(1,4):
+    for c in range(1,4):
+        print(c, end='')
+    print()
+print()
+# sol4
+for r in range(1,5):
+    for c in range(1,r+1):
+        print("*", end='')
+    print()
+print()
+# sol5
+for r in range(1,5):
+    for c in range(1,r+1):
+        print(c, end='')
+    print()
+print()
+# sol6
+for r in range(4,0,-1):
+    for c in range(1,r+1):
+        print(c, end='')
+    print()
+    '''
+# sol7
+for r in range():
+    for c in range():
+        print( ,end='')
+    print()
+'''
+print()
+for r in range(1,5):
+    for c in range(1,5):
+        print(r,end='')
+    print()
+print()
+for r in range(1,5):
+    for c in range(1,5):
+        print(c,end='')
+    print()
+
+
+'''
+#output
+PS C:\Users\Lenovo\OneDrive\Documents\python shit> & "C:/python 3.9/python.exe" "c:/Users/Lenovo/OneDrive/Documents/python shit/patterns.py"
+* * * 
+* * * 
+* * * 
+      
+111   
+222   
+333   
+      
+123   
+123   
+123   
+      
+*     
+**    
+***   
+****  
+      
+1     
+12
+123
+1234
+
+1234
+123
+12
+1
+
+1111
+2222
+3333
+4444
+
+1234
+1234
+1234
+1234
